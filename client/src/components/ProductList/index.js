377be4e48c87e051228c8d08bb15026486e98c4c
Code Wolf <brgerner@gmail.com>
2021-08-09 12:36:14 -0700

    idb

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,6 +5,7 @@ import { from, useQuery } from '@apollo/client';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
   
@@ -20,8 +21,24 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      // let's take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [data, dispatch]);
+    // add else if to check if `loading` is undefined in `useQuery()` Hook
+    else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
